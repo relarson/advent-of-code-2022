e38e631a9f8ba64110e25b75c9e5a0ec04a30b7a
Ross Larson <rossl@remitly.com>
2022-12-02 10:19:43 -0800

    Day 1 & 2

--- aoc/aoc_1/src/main.rs
+++ aoc/aoc_1/src/main.rs
@@ -1,5 +1,5 @@
 use aoc_util;
-use aoc_util::get_numeric_input;
+use aoc_util::get_grouped_numeric_input;
 
 fn main() {
     part1();
@@ -7,11 +7,37 @@ fn main() {
 }
 
 fn part1() {
-    //let nums : Vec<u32> = get_numeric_input("1");
+    let groups : Vec<Vec<u32>> = get_grouped_numeric_input("1");
 
-    println!("Part 1: Answer Not found");
+    let most = sort_calories(groups)[0];
+
+    println!("Part 1: {}", most);
 }
 
 fn part2() {
-    println!("Part 2: Answer Not found");
+    let groups : Vec<Vec<u32>> = get_grouped_numeric_input("1");
+
+    let calories = sort_calories(groups);
+
+    let top3 = calories[0] + calories[1] + calories[2];
+
+    println!("Part 2: {}", top3);
+}
+
+fn sort_calories(groups: Vec<Vec<u32>>) -> Vec<u32> {
+
+    let mut calorie_counts = Vec::<u32>::new();
+
+    for group in &groups {
+        let mut calories = 0;
+        for food in group {
+            calories += food;
+        }
+        calorie_counts.push(calories)
+    }
+
+    calorie_counts.sort();
+    calorie_counts.reverse();
+
+    calorie_counts
 }
--- aoc/aoc_util/src/lib.rs
+++ aoc/aoc_util/src/lib.rs
@@ -17,12 +17,28 @@ pub fn get_input(day: &str) -> Vec<String> {
     for line_res in br.lines() {
         match line_res {
             Ok(line) => lines.push(line),
-            Err(e) => panic!(e)
+            Err(e) => panic!("{}", e)
         }
     }
 
     lines
 }
+
+pub fn get_space_delimited_input(day: &str) -> Vec<Vec<String>> {
+    let lines = get_input(day);
+
+    let mut output = Vec::new();
+    for line in &lines {
+        let mut delimited = Vec::new();
+        for word in line.split_whitespace() {
+            delimited.push(String::from(word))
+        }
+        output.push(delimited)
+    }
+
+    output
+}
+
 
 pub fn get_numeric_input<T>(day: &str) -> Vec<T>
     where T: Add + Product + FromStr,
@@ -39,21 +55,24 @@ pub fn get_numeric_input<T>(day: &str) -> Vec<T>
     nums
 }
 
-/*fn get_input_from_web(day: &str) -> String {
-    let mut bytes = Vec::new();
-    let mut easy = Easy::new();
-    let url = "https://adventofcode.com/2020/day/".to_string() + day + "/input";
-    easy.url(url.as_str()).unwrap();
+pub fn get_grouped_numeric_input<T>(day: &str) -> Vec<Vec<T>>
+    where T: Add + Product + FromStr,
+        <T as FromStr>::Err: Debug
+{
+    let input = get_input(day);
+    let mut groups = Vec::new();
 
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            bytes.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
+    let mut current = Vec::new();
+
+    for line in &input {
+        if line.is_empty() {
+            groups.push(current);
+            current = Vec::new();
+        } else {
+            let n = line.parse::<T>().unwrap();
+            current.push(n);
+        }
     }
 
-    let s = String::from_utf8(bytes).expect("Found invalid UTF-8");
-    s
-}*/
+    groups
+}
